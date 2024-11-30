8464d23f3266eedd7ce19ff93eee0ce7a897ae5c
Christian Duvholt <christian@duvholt.net>
2024-11-30 13:13:42 +0100

    Fix tests in generated code

--- new_day.sh
+++ new_day.sh
@@ -11,9 +11,11 @@ echo "Creating day $DAY"
 
 cp -r input/example "input/$DAY"
 cp -r src/example.rs "src/day$DAY.rs"
+
+sed -i "s/input\/example\/real/input\/$DAY\/real/g" "src/day$DAY.rs"
+
 sed -i "/pub mod example;/a \
 pub mod day$DAY;" src/lib.rs
-
 
 sed -i "/map\.insert(\"example-2\", example::part2);/a \
     map.insert(\"$DAY-1\", day$DAY::part1); \
