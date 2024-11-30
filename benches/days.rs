20edc559cc6c1c7562c386e304a75da4c6ee046e
Christian Duvholt <christian@duvholt.net>
2024-11-30 10:30:40 +0100

    Copying setup from 2022

---
+++ benches/days.rs
@@ -0,0 +1,20 @@
+use adventof2024::day_tasks;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::fs;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let tasks = day_tasks();
+    for (name, func) in tasks.into_iter() {
+        let mut split = name.split("-");
+        let day = split.next().unwrap();
+        let part = split.next().unwrap();
+
+        let contents = fs::read_to_string(format!("./input/{}/real.txt", day)).unwrap();
+        c.bench_function(&format!("day {} part {}", day, part), |b| {
+            b.iter(|| func(black_box(contents.clone())))
+        });
+    }
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
