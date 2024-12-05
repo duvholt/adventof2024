71f0a5fbb37842110433ccb30e44ac30a75d4230
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-05 09:26:09 +0100

    Reuse day 5

--- src/day5.rs
+++ src/day5.rs
@@ -5,40 +5,39 @@ use std::{
 
 pub fn part1(contents: String) -> String {
     let mut parts = contents.split("\n\n");
-    let mut before_order_map: HashMap<u64, HashSet<u64>> = HashMap::new();
-    for order in parts.next().unwrap().lines() {
-        dbg!(order);
-        let mut o = order.split("|");
-        let before = o.next().unwrap().parse().unwrap();
-        let after = o.next().unwrap().parse().unwrap();
-        let a = before_order_map.entry(after).or_default();
-        a.insert(before);
-    }
+    let before_order_map = parse_ordering(&mut parts);
     let mut sum = 0;
     for update in parts.next().unwrap().lines() {
-        let update: Vec<u64> = update.split(",").map(|u| u.parse().unwrap()).collect();
-        let mut failed = false;
-        for (i, u) in update.iter().enumerate() {
-            if let Some(before) = before_order_map.get(u) {
-                let not_before = update.iter().skip(i + 1).all(|u| !before.contains(u));
-                if !not_before {
-                    println!("Fail!");
-                    failed = true;
-                    break;
-                }
-            }
-        }
+        let update = parse_update(update);
+        let failed = is_ordered(&update, &before_order_map);
         if !failed {
             let middle = update[update.len() / 2];
-            dbg!(update, middle);
             sum += middle;
         }
     }
     sum.to_string()
 }
 
-pub fn part2(contents: String) -> String {
-    let mut parts = contents.split("\n\n");
+fn is_ordered(update: &[u64], before_order_map: &HashMap<u64, HashSet<u64>>) -> bool {
+    let mut failed = false;
+    for (i, u) in update.iter().enumerate() {
+        if let Some(before) = before_order_map.get(u) {
+            let not_before = update.iter().skip(i + 1).all(|u| !before.contains(u));
+            if !not_before {
+                failed = true;
+                break;
+            }
+        }
+    }
+    failed
+}
+
+fn parse_update(update: &str) -> Vec<u64> {
+    let update: Vec<u64> = update.split(",").map(|u| u.parse().unwrap()).collect();
+    update
+}
+
+fn parse_ordering(parts: &mut std::str::Split<'_, &str>) -> HashMap<u64, HashSet<u64>> {
     let mut before_order_map: HashMap<u64, HashSet<u64>> = HashMap::new();
     for order in parts.next().unwrap().lines() {
         let mut o = order.split("|");
@@ -47,19 +46,16 @@ pub fn part2(contents: String) -> String {
         let a = before_order_map.entry(after).or_default();
         a.insert(before);
     }
+    before_order_map
+}
+
+pub fn part2(contents: String) -> String {
+    let mut parts = contents.split("\n\n");
+    let before_order_map = parse_ordering(&mut parts);
     let mut sum = 0;
     for update in parts.next().unwrap().lines() {
-        let update: Vec<u64> = update.split(",").map(|u| u.parse().unwrap()).collect();
-        let mut failed = false;
-        for (i, u) in update.iter().enumerate() {
-            if let Some(before) = before_order_map.get(u) {
-                let not_before = update.iter().skip(i + 1).all(|u| !before.contains(u));
-                if !not_before {
-                    failed = true;
-                    break;
-                }
-            }
-        }
+        let update = parse_update(update);
+        let failed = is_ordered(&update, &before_order_map);
         if failed {
             let new = sort_update(&update, &before_order_map);
 
