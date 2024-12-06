a1e49b78195d1158ed7fe39cc67dee362777b223
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-06 09:04:41 +0100

    Slow day 6 part 2

--- src/day6.rs
+++ src/day6.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+#[derive(Eq, PartialEq, Hash, Clone)]
 enum Direction {
     Up,
     Down,
@@ -57,8 +58,67 @@ pub fn part1(contents: String) -> String {
     visited.len().to_string()
 }
 
-pub fn part2(_contents: String) -> String {
-    "example2".to_string()
+pub fn part2(contents: String) -> String {
+    let grid: Vec<Vec<char>> = contents.lines().map(|l| l.chars().collect()).collect();
+    let max_x: isize = grid[0].len() as isize;
+    let max_y: isize = grid.len() as isize;
+    let mut start_position: (isize, isize) = (0, 0);
+
+    for y in 0..max_y {
+        for x in 0..max_x {
+            if grid[y as usize][x as usize] == '^' {
+                start_position = (x, y);
+                break;
+            }
+        }
+    }
+
+    let mut loops = 0;
+
+    for y in 0..max_y {
+        for x in 0..max_x {
+            // skip start
+            if start_position.0 == x && start_position.1 == y {
+                continue;
+            }
+            let mut position = start_position;
+            let mut direction = Direction::Up;
+            let mut looped = false;
+            let mut visited_with_dir = HashSet::new();
+
+            loop {
+                let pos_with_dir = (position, direction.clone());
+                if visited_with_dir.contains(&pos_with_dir) {
+                    // loopy
+                    looped = true;
+                    break;
+                }
+                visited_with_dir.insert(pos_with_dir);
+                let rel = direction_to_rel(&direction);
+                let new_pos = ((position.0 + rel.0) as isize, (position.1 + rel.1) as isize);
+                if new_pos.0 < 0 || new_pos.0 >= max_x || new_pos.1 < 0 || new_pos.1 >= max_y {
+                    break;
+                }
+                let infront = grid[new_pos.1 as usize][new_pos.0 as usize];
+                if infront == '#' || new_pos == (x, y) {
+                    direction = match direction {
+                        Direction::Up => Direction::Right,
+                        Direction::Right => Direction::Down,
+                        Direction::Down => Direction::Left,
+                        Direction::Left => Direction::Up,
+                    }
+                } else {
+                    position = new_pos;
+                }
+            }
+            if looped {
+                loops += 1;
+            }
+        }
+    }
+
+    // print_map(grid, &visited_with_dir);
+    loops.to_string()
 }
 
 fn print_map(grid: Vec<Vec<char>>, visited: &HashSet<(isize, isize)>) {
