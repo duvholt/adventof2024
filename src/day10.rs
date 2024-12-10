24ace952a570a967fd49b2b1a8ab123c9dbed4f9
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-10 06:34:37 +0100

    Day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -6,14 +6,7 @@ pub fn part1(contents: String) -> String {
         .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u64).collect())
         .collect();
 
-    let mut start_points = Vec::new();
-    for y in 0..grid.len() {
-        for x in 0..grid[0].len() {
-            if grid[y][x] == 0 {
-                start_points.push((x, y));
-            }
-        }
-    }
+    let start_points = find_start_points(&grid);
 
     let mut sum = 0;
 
@@ -27,7 +20,7 @@ pub fn part1(contents: String) -> String {
             if visited.contains(&point) {
                 continue;
             }
-            visited.insert(point.clone());
+            visited.insert(point);
             if grid[point.1][point.0] == 9 {
                 peaks += 1;
             }
@@ -40,6 +33,57 @@ pub fn part1(contents: String) -> String {
     }
 
     sum.to_string()
+}
+
+pub fn part2(contents: String) -> String {
+    let grid: Vec<Vec<u64>> = contents
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as u64).collect())
+        .collect();
+
+    let start_points = find_start_points(&grid);
+
+    let mut sum = 0;
+
+    for start_point in start_points {
+        let mut peaks = 0;
+
+        let mut stack = Vec::new();
+        let mut visited = HashSet::new();
+        stack.push((start_point, Vec::new()));
+        while let Some(point_with_path) = stack.pop() {
+            if visited.contains(&point_with_path) {
+                continue;
+            }
+            visited.insert(point_with_path.clone());
+            let (point, path) = point_with_path.clone();
+            if grid[point.1][point.0] == 9 {
+                peaks += 1;
+                continue;
+            }
+            let mut new_path = path;
+            new_path.push(point);
+            for n in neighbourhood(&grid, point) {
+                stack.push((n, new_path.clone()));
+            }
+        }
+
+        sum += peaks;
+    }
+
+    sum.to_string()
+}
+
+fn find_start_points(grid: &[Vec<u64>]) -> Vec<(usize, usize)> {
+    let mut start_points = Vec::new();
+    for y in 0..grid.len() {
+        for x in 0..grid[0].len() {
+            if grid[y][x] == 0 {
+                start_points.push((x, y));
+            }
+        }
+    }
+    start_points
 }
 
 fn neighbourhood(grid: &[Vec<u64>], point: (usize, usize)) -> Vec<(usize, usize)> {
@@ -76,10 +120,6 @@ fn add_if_can_climb(
     }
 }
 
-pub fn part2(_contents: String) -> String {
-    "example2".to_string()
-}
-
 #[cfg(test)]
 mod tests {
     use std::fs;
@@ -90,7 +130,7 @@ mod tests {
     fn test_part1() {
         assert_eq!(
             part1(fs::read_to_string("./input/10/real.txt").unwrap()),
-            "example"
+            "644"
         );
     }
 
@@ -98,7 +138,7 @@ mod tests {
     fn test_part2() {
         assert_eq!(
             part2(fs::read_to_string("./input/10/real.txt").unwrap()),
-            "example2"
+            "1366"
         );
     }
 }
