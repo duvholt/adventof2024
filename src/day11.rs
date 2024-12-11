43ccb52bfbfdf35c3153f0bf038b742d7ec2a822
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-11 07:48:40 +0100

    Clean up a bit

--- src/day11.rs
+++ src/day11.rs
@@ -3,28 +3,32 @@
 // If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
 
 pub fn part1(_contents: String) -> String {
+    solve(_contents, 25)
+}
+
+pub fn part2(_contents: String) -> String {
+    solve(_contents, 75)
+}
+
+fn solve(_contents: String, iterations: usize) -> String {
     let mut stones: Vec<u64> = _contents
         .split_ascii_whitespace()
         .map(|c| c.parse().unwrap())
         .collect();
 
-    for iteration in 0..25 {
+    for iteration in 0..iterations {
         let mut i = 0;
-        while i < stones.len() {
+        let original_len = stones.len();
+        while i < original_len {
             let stone = stones[i];
             if stone == 0 {
                 stones[i] = 1;
                 i += 1;
                 continue;
             }
-            let stone_s = stone.to_string();
-            if stone_s.len() % 2 == 0 {
-                let middle = stone_s.len() / 2;
-                let l = &stone_s[0..middle];
-                let r = &stone_s[middle..];
-                stones.insert(i, l.parse().unwrap());
-                i += 1;
-                stones[i] = r.parse().unwrap();
+            if let Some((l, r)) = can_split(stone) {
+                stones[i] = l;
+                stones.push(r);
                 i += 1;
                 continue;
             } else {
@@ -37,39 +41,15 @@ pub fn part1(_contents: String) -> String {
     stones.len().to_string()
 }
 
-pub fn part2(_contents: String) -> String {
-    let mut stones: Vec<u64> = _contents
-        .split_ascii_whitespace()
-        .map(|c| c.parse().unwrap())
-        .collect();
-
-    for iteration in 0..75 {
-        let mut i = 0;
-        while i < stones.len() {
-            let stone = stones[i];
-            if stone == 0 {
-                stones[i] = 1;
-                i += 1;
-                continue;
-            }
-            let stone_s = stone.to_string();
-            if stone_s.len() % 2 == 0 {
-                let middle = stone_s.len() / 2;
-                let l = &stone_s[0..middle];
-                let r = &stone_s[middle..];
-                stones.insert(i, l.parse().unwrap());
-                i += 1;
-                stones[i] = r.parse().unwrap();
-                i += 1;
-                continue;
-            } else {
-                stones[i] *= 2024;
-                i += 1;
-            }
-        }
-        dbg!(iteration, stones.len());
+fn can_split(stone: u64) -> Option<(u64, u64)> {
+    let stone_s = stone.to_string();
+    if stone_s.len() % 2 == 0 {
+        let middle = stone_s.len() / 2;
+        let l = &stone_s[0..middle];
+        let r = &stone_s[middle..];
+        return Some((l.parse().unwrap(), r.parse().unwrap()));
     }
-    stones.len().to_string()
+    None
 }
 
 #[cfg(test)]
@@ -82,7 +62,7 @@ mod tests {
     fn test_part1() {
         assert_eq!(
             part1(fs::read_to_string("./input/11/real.txt").unwrap()),
-            "example"
+            "193899"
         );
     }
 
