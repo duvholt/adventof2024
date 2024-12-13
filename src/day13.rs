b38da85833f832fa6b8d9cb6b1f5f41784dae2f5
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-13 15:36:20 +0100

    Refactor and reuse solution for both parts

--- src/day13.rs
+++ src/day13.rs
@@ -6,72 +6,53 @@ struct Input {
 }
 
 pub fn part1(contents: String) -> String {
-    let a_cost = 3;
-    let b_cost = 1;
     let machines = parse(contents);
-
-    let mut sum = 0;
-
-    for Input { a, b, prize } in machines {
-        let mut solution = None;
-        for a_presses in 0..100 {
-            let position = (a.0 * a_presses, a.1 * a_presses);
-            let b_presses = (prize.0 - position.0) / b.0;
-
-            if b_presses > 100 {
-                continue;
-            }
-            let calced = (position.0 + (b_presses * b.0), position.1 + b_presses * b.1);
-
-            if calced != prize {
-                continue;
-            }
-            solution = Some((a_presses, b_presses));
-            break;
-        }
-        if let Some((a_presses, b_presses)) = solution {
-            sum += a_presses * a_cost + b_presses * b_cost;
-        }
-    }
-
+    let sum = claw_inputs_sum(machines, 0);
     sum.to_string()
 }
 
 pub fn part2(contents: String) -> String {
+    let machines = parse(contents);
+    let sum = claw_inputs_sum(machines, 10000000000000);
+    sum.to_string()
+}
+
+fn claw_inputs_sum(machines: Vec<Input>, prize_offset: i64) -> i64 {
     let a_cost = 3;
     let b_cost = 1;
-    let machines = parse(contents);
 
     let mut sum = 0;
 
     for Input { a, b, prize } in machines.into_iter() {
-        let mult = 10000000000000;
-        let prize = (prize.0 + mult, prize.1 + mult);
+        let prize = (prize.0 + prize_offset, prize.1 + prize_offset);
 
-        // Cramer's rule
+        let (a_presses, b_presses, calculated_position) = machine_presses(a, b, prize);
 
-        let a1 = a.0;
-        let a2 = a.1;
-
-        let b1 = b.0;
-        let b2 = b.1;
-
-        let c1 = prize.0;
-        let c2 = prize.1;
-
-        let a_presses = (c1 * b2 - b1 * c2) / (a1 * b2 - b1 * a2);
-        let position = (a.0 * a_presses, a.1 * a_presses);
-
-        let b_presses = (prize.0 - position.0) / b.0;
-
-        let calced = (position.0 + (b_presses * b.0), position.1 + b_presses * b.1);
-
-        if calced == prize {
+        if calculated_position == prize {
             sum += a_presses * a_cost + b_presses * b_cost;
         }
     }
+    sum
+}
 
-    sum.to_string()
+fn machine_presses(a: (i64, i64), b: (i64, i64), prize: (i64, i64)) -> (i64, i64, (i64, i64)) {
+    let a1 = a.0;
+    let a2 = a.1;
+
+    let b1 = b.0;
+    let b2 = b.1;
+
+    let c1 = prize.0;
+    let c2 = prize.1;
+
+    // Cramer's rule
+    let a_presses = (c1 * b2 - b1 * c2) / (a1 * b2 - b1 * a2);
+    let position = (a.0 * a_presses, a.1 * a_presses);
+
+    let b_presses = (prize.0 - position.0) / b.0;
+
+    let calculated_position = (position.0 + (b_presses * b.0), position.1 + b_presses * b.1);
+    (a_presses, b_presses, calculated_position)
 }
 
 fn parse(contents: String) -> Vec<Input> {
