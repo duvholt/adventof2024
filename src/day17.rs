1b939d2b221c2574d6aac7dca6481707a5ae0ad7
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-18 18:21:34 +0100

    Use a min heap to ensure that the first solution found is correct

--- src/day17.rs
+++ src/day17.rs
@@ -1,3 +1,5 @@
+use std::collections::BinaryHeap;
+
 use rustc_hash::FxHashSet;
 
 pub fn part1(contents: String) -> String {
@@ -9,42 +11,50 @@ pub fn part1(contents: String) -> String {
     a.join(",")
 }
 
+#[derive(PartialEq, Eq)]
+struct Node(usize);
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        other.0.partial_cmp(&self.0)
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other.0.cmp(&self.0)
+    }
+}
+
 pub fn part2(contents: String) -> String {
     let (instructions, (_register_a, register_b, register_c)) = parse(contents);
 
-    let mut stack: Vec<usize> = Vec::new();
-    stack.push(0);
+    // min heap
+    let mut heap = BinaryHeap::new();
+    heap.push(Node(0));
+
     let mut visited = FxHashSet::default();
-    let mut lowest_register = usize::MAX;
-    while let Some(current_register_a) = stack.pop() {
+    while let Some(Node(current_register_a)) = heap.pop() {
         // solve by finding possible values for bit values left to right
         for byte in 0..8 {
             let new_register_a = (current_register_a << 3) + byte;
-            // skip worse solutions
-            if new_register_a > lowest_register {
-                continue;
-            }
             if visited.contains(&new_register_a) {
                 continue;
             }
             let calculated =
                 run_computer(&instructions, new_register_a as u64, register_b, register_c);
             if instructions == calculated {
-                // found solution, but there might be even better solutions
-                if new_register_a < lowest_register {
-                    lowest_register = new_register_a;
-                }
-                continue;
+                return new_register_a.to_string();
             }
             let correct_instruction = instructions[instructions.len() - calculated.len()];
             let calculated_instruction = calculated[0];
             if correct_instruction == calculated_instruction {
-                stack.push(new_register_a);
+                heap.push(Node(new_register_a));
             }
         }
         visited.insert(current_register_a);
     }
-    lowest_register.to_string()
+    panic!("unable to find a solution")
 }
 
 fn run_computer(
