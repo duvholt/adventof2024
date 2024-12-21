12e8be18aff42d282cc0db99821e4e172895f4e5
Christian Duvholt <christian.duvholt@nrk.no>
2024-12-21 17:37:24 +0100

    Refactor and get rid of move direction type

--- src/day21.rs
+++ src/day21.rs
@@ -1,11 +1,5 @@
 use rustc_hash::FxHashMap;
 
-#[derive(Debug)]
-enum MoveDirection {
-    Vertical,
-    Horizontal,
-}
-
 pub fn part1(contents: String) -> String {
     let rooms: Vec<_> = contents.lines().collect();
 
@@ -13,7 +7,7 @@ pub fn part1(contents: String) -> String {
 
     let mut sum = 0;
     for door_number in rooms {
-        sum += expand_input(door_number, &door_map, &robot_map, 2);
+        sum += count_button_presses_for_door(door_number, &door_map, &robot_map, 2);
     }
 
     sum.to_string()
@@ -26,7 +20,7 @@ pub fn part2(contents: String) -> String {
 
     let mut sum = 0;
     for door_number in rooms {
-        sum += expand_input(door_number, &door_map, &robot_map, 25);
+        sum += count_button_presses_for_door(door_number, &door_map, &robot_map, 25);
     }
 
     sum.to_string()
@@ -61,7 +55,7 @@ fn keypad_maps() -> (
     (door_map, robot_map)
 }
 
-fn expand_input(
+fn count_button_presses_for_door(
     door_number: &str,
     door_map: &FxHashMap<char, (isize, isize)>,
     robot_map: &FxHashMap<char, (isize, isize)>,
@@ -76,44 +70,84 @@ fn expand_input(
     let mut supercount = 0;
 
     for next_key in seq {
-        // find door keypad move
-
-        let (door_move, first_door_moves) = {
+        // Door robot
+        let buttons_to_press = {
             let next_position = door_map[&next_key];
             let door_move = euclidean_move(&current_position, &next_position);
 
-            let first_door_moves =
-                find_first_moves_door(&current_position, next_position, door_move);
+            let start_button_presses =
+                find_possible_start_buttons_door(current_position, next_position, door_move);
 
             current_position = next_position;
-            (door_move, first_door_moves)
+
+            find_robot_keys(robot_map, next_position, door_move, start_button_presses)
         };
 
-        {
-            // Robot 1
-            let keys_to_press1 =
-                find_robot_keys(robot_map, current_position, door_move, first_door_moves);
+        // Middle robots
+        let mut memoized = FxHashMap::default();
 
-            // Middle robots
-            let expanded = keys_to_press1;
-            let mut memoized = FxHashMap::default();
-
-            let count = expand_keys(
-                robot_map,
-                start_robot,
-                &expanded,
-                &mut memoized,
-                times_to_expand,
-            );
-            supercount += count;
-        }
+        let count = expand_keys(
+            robot_map,
+            start_robot,
+            &buttons_to_press,
+            &mut memoized,
+            times_to_expand,
+        );
+        supercount += count;
     }
 
     let num: usize = door_number[0..3].parse().unwrap();
 
-    // dbg!(num, s.len());
-    println!("{} {}", door_number, supercount);
     num * supercount
+}
+
+fn find_possible_start_buttons_door(
+    current_position: (isize, isize),
+    next_position: (isize, isize),
+    door_move: (isize, isize),
+) -> Vec<char> {
+    let mut buttons = vec![];
+
+    if door_move.0 > 0 {
+        buttons.push('>');
+    }
+    if door_move.0 < 0 && !(next_position.0 == 0 && current_position.1 == 3) {
+        buttons.push('<');
+    }
+    if door_move.1 > 0 && !(current_position.0 == 0 && next_position.1 == 3) {
+        buttons.push('v');
+    }
+    if door_move.1 < 0 {
+        buttons.push('^');
+    }
+
+    buttons
+}
+
+fn find_possible_start_buttons_door_robot(
+    current_position: &(isize, isize),
+    next_position: (isize, isize),
+    robot_move: (isize, isize),
+) -> Vec<char> {
+    let mut buttons = vec![];
+
+    if robot_move.0 > 0 {
+        buttons.push('>');
+    }
+    if robot_move.0 < 0 && !(next_position.0 == 0 && current_position.1 == 0) {
+        buttons.push('<');
+    }
+
+    // why does order matter here? if vertical is selected first the result differs
+
+    if robot_move.1 > 0 {
+        buttons.push('v');
+    }
+    if robot_move.1 < 0 && !(current_position.0 == 0 && next_position.1 == 0) {
+        buttons.push('^');
+    }
+
+    buttons
 }
 
 fn expand_keys(
@@ -130,25 +164,17 @@ fn expand_keys(
         return *count;
     }
 
-    let mut current_robot1_position = start_robot;
-    let mut key_group = vec![];
-    for key in start_keys {
-        let (robot_move, robot_first_moves) =
-            robot_move(robot_map, &mut current_robot1_position, *key);
-        key_group.push(find_robot_keys(
-            robot_map,
-            current_robot1_position,
-            robot_move,
-            robot_first_moves,
-        ));
-    }
+    let mut current_robot_position = start_robot;
 
     let mut s = 0;
-    for key_group in key_group.iter() {
+    for key in start_keys {
+        let (robot_move, start_buttons) = robot_move(robot_map, &mut current_robot_position, *key);
+        let key_group =
+            find_robot_keys(robot_map, current_robot_position, robot_move, start_buttons);
         s += expand_keys(
             robot_map,
             start_robot,
-            key_group,
+            &key_group,
             memoized,
             times_to_expand - 1,
         );
@@ -161,69 +187,23 @@ fn robot_move(
     robot_map: &FxHashMap<char, (isize, isize)>,
     current_robot_position: &mut (isize, isize),
     robot_key: char,
-) -> ((isize, isize), Vec<MoveDirection>) {
+) -> ((isize, isize), Vec<char>) {
     let next_position = robot_map[&robot_key];
     let robot_move = euclidean_move(current_robot_position, &next_position);
 
-    let first_moves = find_first_moves_robot(current_robot_position, next_position, robot_move);
+    let start_buttons =
+        find_possible_start_buttons_door_robot(current_robot_position, next_position, robot_move);
     *current_robot_position = next_position;
-    (robot_move, first_moves)
-}
-
-fn find_first_moves_door(
-    current_position: &(isize, isize),
-    next_position: (isize, isize),
-    door_move: (isize, isize),
-) -> Vec<MoveDirection> {
-    let mut first_door_moves = vec![];
-    if next_position.0 == 0 && current_position.1 == 3 {
-        // avoid empty spot
-        first_door_moves.push(MoveDirection::Vertical);
-    } else if current_position.0 == 0 && next_position.1 == 3 {
-        // avoid empty spot
-        first_door_moves.push(MoveDirection::Horizontal);
-    } else {
-        if door_move.0 != 0 {
-            first_door_moves.push(MoveDirection::Horizontal);
-        }
-        if door_move.1 != 0 {
-            first_door_moves.push(MoveDirection::Vertical);
-        }
-    };
-    first_door_moves
-}
-
-fn find_first_moves_robot(
-    current_position: &(isize, isize),
-    next_position: (isize, isize),
-    robot_move: (isize, isize),
-) -> Vec<MoveDirection> {
-    let mut first_moves = vec![];
-    if next_position.0 == 0 && current_position.1 == 0 {
-        // avoid empty spot
-        first_moves.push(MoveDirection::Vertical);
-    } else if current_position.0 == 0 && next_position.1 == 0 {
-        // avoid empty spot
-        first_moves.push(MoveDirection::Horizontal);
-    } else {
-        // why does order matter here? if vertical is selected first the result differs
-        if robot_move.0 != 0 {
-            first_moves.push(MoveDirection::Horizontal);
-        }
-        if robot_move.1 != 0 {
-            first_moves.push(MoveDirection::Vertical);
-        }
-    };
-    first_moves
+    (robot_move, start_buttons)
 }
 
 fn find_robot_keys(
     robot_map: &FxHashMap<char, (isize, isize)>,
     current_position: (isize, isize),
     position_move: (isize, isize),
-    first_moves: Vec<MoveDirection>,
+    start_buttons: Vec<char>,
 ) -> Vec<char> {
-    let next_key = start_key(robot_map, current_position, position_move, first_moves);
+    let next_key = start_key(robot_map, current_position, start_buttons);
     let single_keys = find_key_order(position_move, next_key);
 
     expand_single_keys(position_move, single_keys)
@@ -279,32 +259,30 @@ fn find_key_order(position_move: (isize, isize), next_key: char) -> Vec<char> {
 fn start_key(
     robot_map: &std::collections::HashMap<char, (isize, isize), rustc_hash::FxBuildHasher>,
     current_position: (isize, isize),
-    position_move: (isize, isize),
-    first_moves: Vec<MoveDirection>,
+    possible_start_keys: Vec<char>,
 ) -> char {
-    if first_moves.is_empty() {
+    match possible_start_keys.as_slice() {
         // already at correct key?
-        return 'A';
-    }
-    let possible_start_keys =
-        first_moves
-            .into_iter()
-            .map(|first_door_move| match first_door_move {
-                MoveDirection::Vertical if position_move.1 > 0 => 'v',
-                MoveDirection::Vertical if position_move.1 < 0 => '^',
-                MoveDirection::Horizontal if position_move.0 > 0 => '>',
-                MoveDirection::Horizontal if position_move.0 < 0 => '<',
-                _ => unreachable!(),
-            });
-    // pick key with shortest euclidean distance
+        [] => 'A',
+        [key] => *key,
+        // pick key with shortest euclidean distance
+        [key1, key2] => {
+            let position1 = robot_map[key1];
+            let position2 = robot_map[key2];
+            let robot_move1 = euclidean_move(&current_position, &position1);
+            let robot_move2 = euclidean_move(&current_position, &position2);
 
-    possible_start_keys
-        .min_by_key(|key| {
-            let position = robot_map[key];
-            let robot_move = euclidean_move(&current_position, &position);
-            robot_move.0 + robot_move.1
-        })
-        .unwrap()
+            let cost1 = robot_move1.0 + robot_move1.1;
+            let cost2 = robot_move2.0 + robot_move2.1;
+
+            if cost2 >= cost1 {
+                *key1
+            } else {
+                *key2
+            }
+        }
+        _ => unreachable!(),
+    }
 }
 
 type Position = (isize, isize);
@@ -330,12 +308,11 @@ mod tests {
         );
     }
 
-    #[ignore = "not implemented"]
     #[test]
     fn test_part2() {
         assert_eq!(
             part2(fs::read_to_string("./input/21/real.txt").unwrap()),
-            "example2"
+            "167538833832712"
         );
     }
 }
