85b710ab79812e81df66be9d7a6a25da8487e69d
Christian Duvholt <christian@duvholt.net>
2024-12-23 10:17:01 +0100

    Day 23 part 2

--- src/day23.rs
+++ src/day23.rs
@@ -67,8 +67,50 @@ pub fn part1(contents: String) -> String {
     sum.to_string()
 }
 
-pub fn part2(_contents: String) -> String {
-    "example2".to_string()
+pub fn part2(contents: String) -> String {
+    let edges: Vec<_> = contents
+        .lines()
+        .map(|l| {
+            let mut p = l.split("-");
+            (p.next().unwrap(), p.next().unwrap())
+        })
+        .collect();
+
+    let mut nodes: HashSet<String> = HashSet::new();
+    let mut directed_edges: HashMap<String, HashSet<String>> = HashMap::default();
+    for (from, to) in edges.iter() {
+        nodes.insert(from.to_string());
+        nodes.insert(to.to_string());
+
+        let from_set = directed_edges.entry(from.to_string()).or_default();
+        from_set.insert(to.to_string());
+
+        let to_set = directed_edges.entry(to.to_string()).or_default();
+        to_set.insert(from.to_string());
+    }
+
+    let mut largest_clique = HashSet::default();
+
+    for node in nodes.iter() {
+        let mut remaining_nodes: Vec<_> = nodes.clone().into_iter().collect();
+        let mut clique = HashSet::new();
+        clique.insert(node.to_string());
+        while let Some(vertice) = remaining_nodes.pop() {
+            let edges = directed_edges.get(&vertice).unwrap();
+            if edges.is_superset(&clique) {
+                clique.insert(vertice.to_string());
+            }
+        }
+
+        if clique.len() > largest_clique.len() {
+            largest_clique = clique;
+        }
+    }
+
+    let mut clique_nodes: Vec<_> = largest_clique.into_iter().collect();
+    clique_nodes.sort();
+
+    clique_nodes.join(",")
 }
 
 #[cfg(test)]
