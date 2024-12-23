9721f29e7d2d8150f4b28215cb61e035a9588cd7
Christian Duvholt <christian@duvholt.net>
2024-12-23 18:54:36 +0100

    Cleanup and optimize day 23

--- src/day23.rs
+++ src/day23.rs
@@ -1,104 +1,72 @@
-use std::collections::{HashMap, HashSet};
+use rustc_hash::{FxHashMap, FxHashSet};
 
 pub fn part1(contents: String) -> String {
-    let edges: Vec<_> = contents
-        .lines()
-        .map(|l| {
-            let mut p = l.split("-");
-            (p.next().unwrap(), p.next().unwrap())
-        })
-        .collect();
+    let edges = parse(&contents);
 
-    let mut nodes: HashSet<String> = HashSet::new();
-    let mut directed_edges: HashMap<String, HashSet<String>> = HashMap::default();
-    for (from, to) in edges.iter() {
-        nodes.insert(from.to_string());
-        nodes.insert(to.to_string());
+    let (nodes, directed_edges) = find_nodes_and_edges(edges);
 
-        let from_set = directed_edges.entry(from.to_string()).or_default();
-        from_set.insert(to.to_string());
+    let mut three_cliques = 0;
 
-        let to_set = directed_edges.entry(to.to_string()).or_default();
-        to_set.insert(from.to_string());
-    }
+    let mut visited = FxHashSet::default();
 
-    let mut cliques = Vec::new();
-
-    let mut visited = HashSet::new();
-
-    for (i1, node1) in nodes.iter().enumerate() {
+    for node1 in nodes.iter() {
         let node1_set = directed_edges.get(node1).unwrap();
-        for (i2, node2) in nodes.iter().enumerate() {
-            if i2 <= i1 {
-                continue;
-            }
-            if !node1_set.contains(node2) {
-                continue;
-            }
-            if visited.contains(&(node1, node2)) || visited.contains(&(node2, node1)) {
-                continue;
-            }
-
-            let node2_set = directed_edges.get(node2).unwrap();
-
-            let intersection: Vec<_> = node1_set.intersection(node2_set).collect();
-
+        let starts_with_t1 = node1.starts_with("t");
+        for node2 in node1_set.iter() {
+            let starts_with_t2 = node2.starts_with("t");
             visited.insert((node1, node2));
             visited.insert((node2, node1));
-            for node3 in intersection {
+
+            let node2_set = directed_edges.get(node2).unwrap();
+            let node3_set = node1_set.intersection(node2_set);
+
+            for node3 in node3_set {
+                if !(starts_with_t1 || starts_with_t2 || node3.starts_with("t")) {
+                    continue;
+                }
                 if visited.contains(&(node1, node3)) || visited.contains(&(node2, node3)) {
                     continue;
                 }
-
-                visited.insert((node1, node2));
-                visited.insert((node2, node1));
-                cliques.push((node1, node2, node3));
+                three_cliques += 1;
             }
         }
     }
 
-    let mut sum = 0;
-    for clique in cliques {
-        if clique.0.starts_with("t") || clique.1.starts_with("t") || clique.2.starts_with("t") {
-            sum += 1;
-        }
-    }
+    three_cliques.to_string()
+}
 
-    sum.to_string()
+fn find_nodes_and_edges<'a>(
+    edges: Vec<(&'a str, &'a str)>,
+) -> (FxHashSet<&'a str>, FxHashMap<&'a str, FxHashSet<&'a str>>) {
+    let mut nodes: FxHashSet<&str> = FxHashSet::default();
+    let mut directed_edges: FxHashMap<&str, FxHashSet<&str>> = FxHashMap::default();
+    for (from, to) in edges.iter() {
+        nodes.insert(from);
+        nodes.insert(to);
+
+        let from_set = directed_edges.entry(from).or_default();
+        from_set.insert(to);
+
+        let to_set = directed_edges.entry(to).or_default();
+        to_set.insert(from);
+    }
+    (nodes, directed_edges)
 }
 
 pub fn part2(contents: String) -> String {
-    let edges: Vec<_> = contents
-        .lines()
-        .map(|l| {
-            let mut p = l.split("-");
-            (p.next().unwrap(), p.next().unwrap())
-        })
-        .collect();
+    let edges = parse(&contents);
 
-    let mut nodes: HashSet<String> = HashSet::new();
-    let mut directed_edges: HashMap<String, HashSet<String>> = HashMap::default();
-    for (from, to) in edges.iter() {
-        nodes.insert(from.to_string());
-        nodes.insert(to.to_string());
+    let (nodes, directed_edges) = find_nodes_and_edges(edges);
 
-        let from_set = directed_edges.entry(from.to_string()).or_default();
-        from_set.insert(to.to_string());
+    let mut largest_clique = FxHashSet::default();
 
-        let to_set = directed_edges.entry(to.to_string()).or_default();
-        to_set.insert(from.to_string());
-    }
-
-    let mut largest_clique = HashSet::default();
-
-    for node in nodes.iter() {
-        let mut remaining_nodes: Vec<_> = nodes.clone().into_iter().collect();
-        let mut clique = HashSet::new();
-        clique.insert(node.to_string());
-        while let Some(vertice) = remaining_nodes.pop() {
-            let edges = directed_edges.get(&vertice).unwrap();
+    for node in nodes.iter().cloned() {
+        let mut clique = FxHashSet::default();
+        clique.insert(node);
+        for vertice in nodes.iter() {
+            let edges = directed_edges.get(vertice).unwrap();
             if edges.is_superset(&clique) {
-                clique.insert(vertice.to_string());
+                clique.insert(vertice);
             }
         }
 
@@ -113,6 +81,17 @@ pub fn part2(contents: String) -> String {
     clique_nodes.join(",")
 }
 
+fn parse(contents: &str) -> Vec<(&str, &str)> {
+    let edges: Vec<_> = contents
+        .lines()
+        .map(|l| {
+            let mut p = l.split("-");
+            (p.next().unwrap(), p.next().unwrap())
+        })
+        .collect();
+    edges
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
@@ -123,7 +102,7 @@ mod tests {
     fn test_part1() {
         assert_eq!(
             part1(fs::read_to_string("./input/23/real.txt").unwrap()),
-            "example"
+            "1512"
         );
     }
 
@@ -131,7 +110,7 @@ mod tests {
     fn test_part2() {
         assert_eq!(
             part2(fs::read_to_string("./input/23/real.txt").unwrap()),
-            "example2"
+            "ac,ed,fh,kd,lf,mb,om,pe,qt,uo,uy,vr,wg"
         );
     }
 }
