747ad15ad0f2841c27703efbf73e805dfc9eddba
Christian Duvholt <christian@duvholt.net>
2024-12-26 21:26:47 +0100

    Store path in hashmap instead of node for day 16

--- src/day16.rs
+++ src/day16.rs
@@ -6,17 +6,21 @@ use std::{
 use rustc_hash::{FxHashMap, FxHashSet};
 
 #[derive(Clone, PartialEq, Eq, Debug, Hash)]
-struct Node(Position, Direction, Vec<(Position, u64)>, u64);
+struct Node(
+    (Position, Direction),
+    Option<((Position, Direction), u64)>,
+    u64,
+);
 
 impl PartialOrd for Node {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(other.3.cmp(&self.3))
+        Some(other.2.cmp(&self.2))
     }
 }
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        other.3.cmp(&self.3)
+        other.2.cmp(&self.2)
     }
 }
 
@@ -24,18 +28,18 @@ pub fn part1(contents: String) -> String {
     let (map_grid, start, end) = parse(contents);
     let direction = Direction::Right;
 
-    let node = find_path(start, end, direction, &map_grid);
+    let (node, _) = find_path(start, end, direction, &map_grid, false);
 
-    node.3.to_string()
+    node.2.to_string()
 }
 
 pub fn part2(contents: String) -> String {
     let (map_grid, start, end) = parse(contents);
     let direction = Direction::Right;
 
-    let node = find_path(start, end, direction, &map_grid);
+    let (_, path) = find_path(start, end, direction, &map_grid, true);
 
-    let goals = node.2.into_iter().collect();
+    let goals = path.unwrap().into_iter().collect();
     let all = find_all_paths(start, goals, direction, map_grid);
 
     all.to_string()
@@ -46,30 +50,57 @@ fn find_path(
     end: (usize, usize),
     direction: Direction,
     map_grid: &Vec<Vec<bool>>,
-) -> Node {
-    let node = Node(start, direction, vec![(start, 0)], 0);
+    include_path: bool,
+) -> (Node, Option<Vec<(Position, u64)>>) {
+    let node = Node((start, direction), None, 0);
     let mut frontier = BinaryHeap::new();
     frontier.push(node);
     let mut expanded = HashSet::new();
+    let mut path_map = FxHashMap::default();
 
     while let Some(node) = frontier.pop() {
-        if expanded.contains(&(node.0, node.1)) {
+        if expanded.contains(&node.0) {
             continue;
         }
-        if node.0 == end {
+
+        if include_path {
+            if let Some(prev) = node.1 {
+                path_map.insert(node.0, prev);
+            }
+        }
+        if node.0 .0 == end {
             // won
-            return node;
+            let path = if include_path {
+                Some(traverse_path(&path_map, &node))
+            } else {
+                None
+            };
+            return (node, path);
         }
         let neighbours = neighbourhood(map_grid, &node);
         for neighbour in neighbours {
-            if !expanded.contains(&(neighbour.0, neighbour.1)) {
+            if !expanded.contains(&neighbour.0) {
                 frontier.push(neighbour);
             }
         }
-        expanded.insert((node.0, node.1));
+        expanded.insert(node.0);
         // print_map(&map_grid, position, direction);
     }
     panic!("Unable to find path")
+}
+
+fn traverse_path(
+    path_map: &FxHashMap<(Position, Direction), ((Position, Direction), u64)>,
+    start: &Node,
+) -> Vec<(Position, u64)> {
+    let mut position = &(start.0, start.2);
+    let mut path = vec![(position.0 .0, start.2)];
+    while let Some(next) = path_map.get(&position.0) {
+        path.push((next.0 .0, next.1));
+        position = next;
+    }
+    path.reverse();
+    path
 }
 
 fn find_all_paths(
@@ -78,29 +109,34 @@ fn find_all_paths(
     direction: Direction,
     map_grid: Vec<Vec<bool>>,
 ) -> usize {
-    let node = Node(start, direction, vec![(start, 0)], 0);
+    let node = Node((start, direction), None, 0);
     let mut frontier = BinaryHeap::new();
     frontier.push(node);
     let mut expanded = FxHashMap::default();
 
+    let mut path_map = FxHashMap::default();
     let mut best_path = FxHashSet::default();
 
     while let Some(node) = frontier.pop() {
-        if goals.contains(&(node.0, node.3)) {
-            for (p, _) in &node.2 {
-                best_path.insert(*p);
+        if let Some(cost) = expanded.get(&node.0) {
+            if *cost < node.2 {
+                continue;
             }
         }
-        if let Some(cost) = expanded.get(&(node.0, node.1)) {
-            if *cost < node.3 {
-                continue;
+        if let Some(prev) = node.1 {
+            path_map.insert(node.0, prev);
+        }
+
+        if goals.contains(&(node.0 .0, node.2)) {
+            for p in traverse_path(&path_map, &node) {
+                best_path.insert(p.0);
             }
         }
         let neighbours = neighbourhood(&map_grid, &node);
         for neighbour in neighbours {
             frontier.push(neighbour);
         }
-        expanded.insert((node.0, node.1), node.3);
+        expanded.insert(node.0, node.2);
     }
 
     best_path.len()
@@ -141,26 +177,32 @@ fn print_map_path(
 
 fn neighbourhood(map_grid: &[Vec<bool>], node: &Node) -> Vec<Node> {
     let mut new = Vec::new();
-    let new_directions = match node.1 {
+    let new_directions = match node.0 .1 {
         Direction::Up | Direction::Down => (Direction::Left, Direction::Right),
         Direction::Right | Direction::Left => (Direction::Up, Direction::Down),
     };
-    for new_direction in [new_directions.0, new_directions.1] {
-        new.push(Node(node.0, new_direction, node.2.clone(), node.3 + 1000));
-    }
     let new_position = next_position(node);
     if !map_grid[new_position.1][new_position.0] {
-        let mut path = node.2.clone();
-        path.push((new_position, node.3 + 1));
-        new.push(Node(new_position, node.1, path, node.3 + 1));
+        new.push(Node(
+            (new_position, node.0 .1),
+            Some((node.0, node.2)),
+            node.2 + 1,
+        ));
+    }
+    for new_direction in [new_directions.0, new_directions.1] {
+        new.push(Node(
+            (node.0 .0, new_direction),
+            Some((node.0, node.2)),
+            node.2 + 1000,
+        ));
     }
 
     new
 }
 
 fn next_position(node: &Node) -> (usize, usize) {
-    let position = node.0;
-    let (new_x, new_y) = match node.1 {
+    let position = node.0 .0;
+    let (new_x, new_y) = match node.0 .1 {
         Direction::Up => (position.0, position.1 - 1),
         Direction::Right => (position.0 + 1, position.1),
         Direction::Down => (position.0, position.1 + 1),
